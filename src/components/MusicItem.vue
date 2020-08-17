49dd6354d53e62c2de5aa9150dd54990e0cf8c3f
Ragnar-Oock <u.drgnk@outlook.com>
2020-08-17 22:07:48 +0200

    add clic to copy command moved copy icon to it's own file

--- src/components/MusicItem.vue
+++ src/components/MusicItem.vue
@@ -63,27 +63,29 @@
 								</svg>
 							</button>
 
-							<output class="music-item__output">{{ command }}</output>
+							<input
+								ref="output"
+								class="music-item__output"
+								type="text"
+								:value="command"
+								readonly
+							>
 
 							<button
+								ref="copyButton"
 								class="music-item__button"
 								title="copier la commande"
+								@click="copy"
 							>
 								<svg
 									width="1em"
 									height="1em"
 									viewBox="0 0 16 16"
-									class="bi bi-clipboard-plus"
 									fill="currentColor"
 									xmlns="http://www.w3.org/2000/svg"
 								>
-									<path
-										fill-rule="evenodd"
-										d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1v-1z"
-									/>
-									<path
-										fill-rule="evenodd"
-										d="M9.5 1h-3a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3zM8 7a.5.5 0 0 1 .5.5V9H10a.5.5 0 0 1 0 1H8.5v1.5a.5.5 0 0 1-1 0V10H6a.5.5 0 0 1 0-1h1.5V7.5A.5.5 0 0 1 8 7z"
+									<use
+										xlink:href="../assets/images/copy.svg#copy"
 									/>
 								</svg>
 							</button>
@@ -178,6 +180,15 @@ export default {
 		},
 		toggleVip() {
 			this.vip = !this.vip;
+		},
+		copy() {
+			console.log(this.$refs.output);
+			this.$refs.output.select();
+			document.execCommand('copy');
+
+			this.$nextTick().then(() => {
+				this.$refs.copyButton.focus();
+			});
 		}
 	}
 };
@@ -315,6 +326,7 @@ export default {
 			background-color: #d7d7d7;
 			color: #484848;
 			padding: .5em;
+			border: none;
 		}
 
 		.open-enter-active {
