4bd5dfa1e8381780f8efc01616d4e5d5f5ff37b8
Ragnar-Oock <u.drgnk@outlook.com>
2020-10-16 19:09:03 +0200

    create a language selector component

---
+++ src/components/LangSelector.vue
@@ -0,0 +1,37 @@
+<template>
+	<section class="lang-selector">
+		<select v-model="$i18n.locale">
+			<option
+				v-for="(value, key) in supportedLocales"
+				:key="key"
+				:value="key"
+			>
+				{{ value }}
+			</option>
+		</select>
+	</section>
+</template>
+
+<script lang="js">
+
+import { supportedLocales } from '@/lang/supported';
+
+export default {
+	name: 'LangSelector',
+	data () {
+		return {
+			supportedLocales
+		};
+	}
+};
+
+
+</script>
+
+<style scoped lang="scss">
+  .lang-selector {
+		&__button {
+			border: groove 5px lime;
+		}
+  }
+</style>
