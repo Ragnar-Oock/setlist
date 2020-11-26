431e42fe03255e5aff8fdb5540d99b02d9682603
Ragnar-Oock <u.drgnk@outlook.com>
2020-11-26 18:48:54 +0100

    update action to use swagger client

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,11 +1,17 @@
-import { query } from './api/swagger';
+import SwaggerClient from 'swagger-client';
+
+
 
 const actions = {
 	async getSongList({ commit }) {
 		try {
-			const { data } = await query({ operationId:'songs.get' });
-
-			commit('SET_SONG_LIST', data.data);
+			console.log('%c setlist %c API %c   Loading songs', 'background: #583A94; color:#fff; border-radius:3px 0 0 3px', 'background: lightblue; color:#404040; border-radius:0 3px 3px 0', 'background: none');
+			new SwaggerClient(process.env.VUE_APP_SWAGGER_PATH)
+				.then(client => client.apis.public.songs_get())
+				.then(response => {
+					commit('SET_SONG_LIST', response.obj.data);
+					commit('INCREMENT_PAGE');
+				});
 		}
 		catch (error) {
 			console.error(error);
@@ -13,11 +19,20 @@ const actions = {
 	},
 	async getMoreSongs({ commit, getters }) {
 		try {
-			// eslint-disable-next-line no-useless-concat
-			console.log('%c setlist %c   Loading more songs', 'background: #583A94; color:#fff; border-radius:3px', 'background: none');
-			const { data } = await query({ operationId:'songs.get', data:{ padding: getters.getPage * 50 } });
+			if (getters.getPage > 0) {
+				console.log('%c setlist %c API %c   Loading more songs', 'background: #583A94; color:#fff; border-radius:3px 0 0 3px', 'background: lightblue; color:#404040; border-radius:0 3px 3px 0', 'background: none');
+				// const { data } = await query({ operationId:'songs.get', data:{ padding: getters.getPage * 50 } });
+				new SwaggerClient(process.env.VUE_APP_SWAGGER_PATH)
+					.then(client => client.apis.public.songs_get({ padding: getters.getPage * 50 }))
+					.then(response => {
 
-			commit('ADD_SONGS_TO_LIST', data.data);
+						commit('ADD_SONGS_TO_LIST', response.obj.data);
+						commit('INCREMENT_PAGE');
+					});
+			}
+			else {
+				console.log('%c setlist %c API %c   no songs loaded waiting for first page', 'background: #583A94; color:#fff; border-radius:3px 0 0 3px', 'background: lightblue; color:#404040; border-radius:0 3px 3px 0', 'background: none');
+			}
 		}
 		catch (error) {
 			console.error(error);
