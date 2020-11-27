d4ece91476caddbd758ba1475196abfd953d0c69
Ragnar-Oock <u.drgnk@outlook.com>
2020-11-27 19:12:16 +0100

    cache the API Client
    add a client getter that cache the API Client to avoid recomputation of the spec for each request
    update actions to use the async await syntax instead of promise syntaxe

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,17 +1,32 @@
 import SwaggerClient from 'swagger-client';
 
 
+function getClient() {
+	return new Promise((resolve, reject) => {
+		if (getClient.cachedClient) {
+			resolve(getClient.cachedClient);
+		}
+		else {
+			new SwaggerClient(process.env.VUE_APP_SWAGGER_PATH)
+				.then(client => {
+					getClient.cachedClient = client;
+					resolve(client);
+				})
+				.catch(e => reject(e));
+		}
+	});
+}
 
 const actions = {
 	async getSongList({ commit }) {
 		try {
 			console.log('%c setlist %c API %c   Loading songs', 'background: #583A94; color:#fff; border-radius:3px 0 0 3px', 'background: lightblue; color:#404040; border-radius:0 3px 3px 0', 'background: none');
-			new SwaggerClient(process.env.VUE_APP_SWAGGER_PATH)
-				.then(client => client.apis.public.songs_get())
-				.then(response => {
-					commit('SET_SONG_LIST', response.obj.data);
-					commit('INCREMENT_PAGE');
-				});
+			const client = await getClient();
+
+			const response = await client.apis.public.songs_get();
+
+			commit('SET_SONG_LIST', response.obj.data);
+			commit('INCREMENT_PAGE');
 		}
 		catch (error) {
 			console.error(error);
@@ -21,14 +36,14 @@ const actions = {
 		try {
 			if (getters.getPage > 0) {
 				console.log('%c setlist %c API %c   Loading more songs', 'background: #583A94; color:#fff; border-radius:3px 0 0 3px', 'background: lightblue; color:#404040; border-radius:0 3px 3px 0', 'background: none');
-				// const { data } = await query({ operationId:'songs.get', data:{ padding: getters.getPage * 50 } });
-				new SwaggerClient(process.env.VUE_APP_SWAGGER_PATH)
-					.then(client => client.apis.public.songs_get({ padding: getters.getPage * 50 }))
-					.then(response => {
 
-						commit('ADD_SONGS_TO_LIST', response.obj.data);
-						commit('INCREMENT_PAGE');
-					});
+				const client = await getClient();
+
+				const response = await client.apis.public.songs_get({ padding: getters.getPage * 50 });
+
+				commit('ADD_SONGS_TO_LIST', response.obj.data);
+				commit('INCREMENT_PAGE');
+
 			}
 			else {
 				console.log('%c setlist %c API %c   no songs loaded waiting for first page', 'background: #583A94; color:#fff; border-radius:3px 0 0 3px', 'background: lightblue; color:#404040; border-radius:0 3px 3px 0', 'background: none');
