be4a39e5b2edbce3a9ac7cd0c41c855a94b1ab2f
Ragnar-Oock <u.drgnk@outlook.com>
2020-12-01 21:18:49 +0100

    add a compareKeys and GetDiffKeyshelper functions

--- src/helpers/methods.js
+++ src/helpers/methods.js
@@ -8,11 +8,65 @@
  * @param {String} styles.tag style for the tag
  * @param {String} styles.message style for the message
  */
-function prettyLog(module, tag, message, styles) {
+export function prettyLog(module, tag, message, styles) {
 	styles = { ...{ module:'background: #583A94; color:#fff; border-radius:3px 0 0 3px',
 		tag: 'background: lightblue; color:#404040; border-radius:0 3px 3px 0',
 		message: 'background: none' }, ...styles };
 	console.log(`%c ${ module } %c ${ tag } %c   ${ message }`, styles.module, styles.tag, styles.message);
 }
 
-export default prettyLog;
+export function compareKeys(requiredKeys, providedKeys) {
+	try {
+		let keysAreIdenticals = false;
+		const _requiredKeys = Object.entries(requiredKeys);
+
+		_requiredKeys.some(entry => {
+			const key = entry[0];
+
+			if (typeof providedKeys[key] === 'object') {
+				keysAreIdenticals = compareKeys(providedKeys[key], requiredKeys[key]);
+			}
+			else {
+				keysAreIdenticals = (providedKeys[key] === requiredKeys[key]);
+			}
+
+			return !keysAreIdenticals;
+		});
+
+		return keysAreIdenticals;
+	}
+	catch (error) {
+		prettyLog('setlist', 'helpers', error);
+		throw new Error(error);
+	}
+}
+
+export function getDiffKeys(requiredKeys, providedKeys) {
+	try {
+		const diffKeys = [];
+		const _requiredKeys = Object.entries(requiredKeys);
+
+		_requiredKeys.forEach(entry => {
+			let areKeysDifferent = false;
+			const key = entry[0];
+
+			if (typeof providedKeys[key] === 'object') {
+				areKeysDifferent = !compareKeys(providedKeys[key], requiredKeys[key]);
+			}
+			else {
+				areKeysDifferent = (providedKeys[key] !== requiredKeys[key]);
+			}
+
+			if (areKeysDifferent) {
+				diffKeys.push(key);
+			}
+		});
+
+		return diffKeys;
+	}
+	catch (error) {
+		prettyLog('setlist', 'helpers', error);
+		throw new Error(error);
+	}
+}
+
