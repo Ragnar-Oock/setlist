cd58f5f7b6dfdc917e171c307ca22ee6624afdbd
Ragnar-Oock <u.drgnk@outlook.com>
2020-12-04 18:42:40 +0100

    prevent pretty log in prod

--- src/helpers/methods.js
+++ src/helpers/methods.js
@@ -9,10 +9,12 @@
  * @param {String} styles.message style for the message
  */
 export function prettyLog(module, tag, message, styles) {
-	styles = { ...{ module:'background: #583A94; color:#fff; border-radius:3px 0 0 3px',
-		tag: 'background: lightblue; color:#404040; border-radius:0 3px 3px 0',
-		message: 'background: none' }, ...styles };
-	console.log(`%c ${ module } %c ${ tag } %c   ${ message }`, styles.module, styles.tag, styles.message);
+	if (process.env.VUE_APP_SHOW_LOG) {
+		styles = { ...{ module:'background: #583A94; color:#fff; border-radius:3px 0 0 3px',
+			tag: 'background: lightblue; color:#404040; border-radius:0 3px 3px 0',
+			message: 'background: none' }, ...styles };
+		console.log(`%c ${ module } %c ${ tag } %c   ${ message }`, styles.module, styles.tag, styles.message);
+	}
 }
 
 export function compareKeys(requiredKeys, providedKeys) {
