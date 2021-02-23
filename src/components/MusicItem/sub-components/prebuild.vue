4cd353659ffd9150b5d96355ea95ed3e7edb9237
Ragnar-Oock <u.drgnk@outlook.com>
2021-02-23 18:07:24 +0100

    add icons on prebuild buttons

---
+++ src/components/MusicItem/sub-components/prebuild.vue
@@ -0,0 +1,38 @@
+				class="btn small dull with-icon"
+				<img
+					v-if="vip"
+					svg-inline
+					class="icon"
+					src="@/assets/images/diamond-fill.svg"
+					aria-hidden="true"
+				>
+				<img
+					v-else
+					svg-inline
+					class="icon"
+					src="@/assets/images/diamond.svg"
+					aria-hidden="true"
+				>
+				class="btn small dull with-icon"
+				<img
+					v-if="edit"
+					svg-inline
+					class="icon"
+					src="@/assets/images/edit-fill.svg"
+					aria-hidden="true"
+				>
+				<img
+					v-else
+					svg-inline
+					class="icon"
+					src="@/assets/images/edit.svg"
+					aria-hidden="true"
+				>
+		<div class="btn-group copy">
+			<button class="btn small dull with-icon">
+				<img
+					svg-inline
+					class="icon"
+					src="@/assets/images/clipboard-plus.svg"
+					aria-hidden="true"
+				>
