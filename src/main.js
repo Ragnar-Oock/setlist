83e8891d4d7ce6c61389368b99d255bc5fa01e05
Ragnar-Oock <u.drgnk@outlook.com>
2021-03-18 22:13:26 +0100

    setup sweetalert

--- src/main.js
+++ src/main.js
@@ -30,6 +30,11 @@ import PortalVue from 'portal-vue';
 
 Vue.use(PortalVue);
 
+// modal
+import VueSweetalert2 from 'vue-sweetalert2';
+import 'sweetalert2/dist/sweetalert2.min.css';
+Vue.use(VueSweetalert2);
+
 // enable devtools in prod
 Vue.config.devtools = true;
 
